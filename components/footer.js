0608fa7a8300a516e960e67229752584ce2a22d3
thisiswoo <thisiswoo0594@gmail.com>
2022-09-16 17:33:04 +0900

    modify footer add rel=noreferrer

--- components/footer.js
+++ components/footer.js
@@ -11,7 +11,7 @@ import {
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} This website is built based on the <a href='https://www.craftz.dog/' target='_blank'>Takuya Matsuyama&apos;s website</a>.
+      &copy; {new Date().getFullYear()} This website is built based on the <a href='https://www.craftz.dog/' target='_blank' rel="noreferrer">Takuya Matsuyama&apos;s website</a>.
       <br/> As a big fan of yours living in Korea, I feel grateful to you.
       {/*<TbBrandGithub /><TbBrandGmail /><TbBrandInstagram />*/}
     </Box>
