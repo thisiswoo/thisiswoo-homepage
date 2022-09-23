940af924829003254afc1a20c7bd33902f49b548
thisiswoo <thisiswoo0594@gmail.com>
2022-09-23 10:39:48 +0900

    edit navbar

--- components/navbar.js
+++ components/navbar.js
@@ -100,13 +100,10 @@ const Navbar = props => {
             <IoLogoGithub />
             Github
           </LinkItem>
-
         </Stack>
 
         <Box flex={1} align="right">
-
           <ThemeToggleButton />
-
           <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
             <Menu>
               <MenuButton
