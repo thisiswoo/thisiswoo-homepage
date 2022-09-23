c9f1afb2c34512842b22d4d564171f7249a60d30
thisiswoo <thisiswoo0594@gmail.com>
2022-09-23 10:53:17 +0900

    Revert "edit index.js"
    This reverts commit 3c3994a7df48f3f3df9e5d51b49bb1d1cf2ca4fa.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "37b6e5bd4f0796eb"; }
+/******/ 		__webpack_require__.h = function() { return "68880f2bfff7cb1c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
